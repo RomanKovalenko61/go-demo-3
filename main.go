e63225a328621c26572212baf9074e159a815789
Roman <astek14@mail.ru>
2025-04-10 15:38:04 +0300

    7.2 work with map

--- main.go
+++ main.go
@@ -4,7 +4,17 @@ import "fmt"
 
 func main() {
 	m := map[string]string{
-		"PurpleSchool": "https://purpleschool.ru",
+		"PurpleSchool": "purpleschool.ru",
 	}
+	fmt.Println(m)
+	fmt.Println(m["PurpleSchool"])
+	m["PurpleSchool"] = "https://purpleschool.ru"
+	fmt.Println(m["PurpleSchool"])
+	m["Google"] = "https://google.com"
+	m["Yandex"] = "https://yandex.ru"
+	fmt.Println(m)
+	delete(m, "Yandex")
+	delete(m, "Y") // delete not exist
+	fmt.Println(m["Y"]) // empty
 	fmt.Println(m)
 }
