2b7b9c8a67b66e7a8b24e9e39e51c5b3074c6552
Roman <astek14@mail.ru>
2025-04-10 15:42:11 +0300

    7.3 Iteration map

--- main.go
+++ main.go
@@ -3,18 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	m := map[string]string{
-		"PurpleSchool": "purpleschool.ru",
+	m := map[string]int{"a": 1, "b": 2}
+	for key, value := range m {
+		fmt.Println(key, value)
 	}
-	fmt.Println(m)
-	fmt.Println(m["PurpleSchool"])
-	m["PurpleSchool"] = "https://purpleschool.ru"
-	fmt.Println(m["PurpleSchool"])
-	m["Google"] = "https://google.com"
-	m["Yandex"] = "https://yandex.ru"
-	fmt.Println(m)
-	delete(m, "Yandex")
-	delete(m, "Y") // delete not exist
-	fmt.Println(m["Y"]) // empty
-	fmt.Println(m)
 }
